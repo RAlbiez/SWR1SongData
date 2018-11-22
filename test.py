d739a08693a8a828102aea35db79e79a157a9e48
Albiez <ral@nissen-velten.de>
2018-11-22 15:50:49 +0100

    added file outputs, run timing and finding next links

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-
+import time
 
 # data entry class
 # time: time song was played
 # songName: name of song
 # artist: artist
+
+
 class entry:
     # constructor
     def __init__(self, timeP, songNameP, artistP):
@@ -23,60 +25,117 @@ class entry:
     artist = ''
 
 
-# get current time
-timeNow = datetime.today() - timedelta(hours=1)
-timeStr = str(timeNow.year) + str(timeNow.month) + \
-    str(timeNow.day) + str(timeNow.hour)
+# keep this thing running all the time
+while 1:
 
-# put together url
-url = 'https://www.swr.de/swr1/bw/musikrecherche/-/id=446260/date=' + \
-    timeStr + '/did=13840668/nid=446260/1iptl4o/index.html'
+    # determine time for next run: 5 minutes past next hour
+    nextRun = datetime.today() + timedelta(hours=1)
+    nextRun = nextRun.replace(minute=5)
 
-# !!!Warning!!!
-print("warning, url broken at this point, manually adjust for testing")
+    # wait for next run
+    while datetime.now() < nextRun:
+        time.sleep(60)
 
-# header for request
-headerString = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
- Chrome / 41.0 .2272 .101 Safari / 537.36 '
+    # read url to read from
+    with open("nextHour.txt", "r") as f:
+        url = f.readline()
 
-# new session for request
-session = requests.Session()
+    # header for request
+    headerString = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
+    Chrome / 41.0 .2272 .101 Safari / 537.36 '
 
-headers = {'User-Agent': headerString}
+    # new session for request
+    session = requests.Session()
 
-# perform get request
-contents = session.get(url, headers=headers)
+    headers = {'User-Agent': headerString}
 
-# read encoding of contents
-encoding = contents.encoding if 'charset' in contents.headers.get(
-    'content-type', '').lower() else None
+    # perform get request
+    contents = session.get(url, headers=headers)
 
-# create BeatifulSoup Html reader instance with correct encoding
-soup = BeautifulSoup(
-    contents.content, from_encoding=encoding, features="html.parser")
+    # write warning in error case
+    if contents.status_code != 200:
+        errorMessage = 'No correct start URL was supplied, please create a file named\
+         "nextHour.txt" in the same folder as the executable and place a working URL in it!'
+        print(errorMessage)
 
-# find all li tags from class "musicListItem"
-liItems = soup.find_all("li", {"class": "musicListItem"})
+    # read encoding of contents
+    encoding = contents.encoding if 'charset' in contents.headers.get(
+        'content-type', '').lower() else None
 
-# get the text without html tags contained in these items
-results = [i.text for i in liItems]
+    # create BeatifulSoup Html reader instance with correct encoding
+    soup = BeautifulSoup(
+        contents.content, from_encoding=encoding, features="html.parser")
 
-# parse the result into  entry objects
-songEntrys = []
-for unchangedData in results:
-    # split line at newline char to find the relevant lines
-    dataLines = unchangedData.split('\n')
-    relevantLines = []
-    for line in dataLines:
-        # remove whitespace
-        lineStripped = line.strip()
-        # add line to result if there is still text in line
-        if lineStripped != '':
-            relevantLines.append(lineStripped)
-    # create entry object for line
-    obj = entry(relevantLines[0], relevantLines[1], relevantLines[2])
-    songEntrys.append(obj)
+    # find all li tags from class "musicListItem"
+    liItems = soup.find_all("li", {"class": "musicListItem"})
 
-# print it to console for debug purposes
-for obj in songEntrys:
-    print(obj)
+    # get the text without html tags contained in these items
+    results = [i.text for i in liItems]
+
+    # parse the result into  entry objects
+    songEntrys = []
+    for unchangedData in results:
+        # split line at newline char to find the relevant lines
+        dataLines = unchangedData.split('\n')
+        relevantLines = []
+        for line in dataLines:
+            # remove whitespace
+            lineStripped = line.strip()
+            # add line to result if there is still text in line
+            if lineStripped != '':
+                relevantLines.append(lineStripped)
+        # create entry object for line
+        obj = entry(relevantLines[0], relevantLines[1], relevantLines[2])
+        songEntrys.append(obj)
+
+    # get current time
+    timeNow = datetime.today()
+
+    # make a new file every month
+    resultFileName = 'SWR1_History_' + \
+        str(timeNow.year) + '_' + str(timeNow.month) + '.txt'
+
+    # print result to file
+    with open(resultFileName, 'a') as file:
+        for obj in songEntrys:
+            file.write(str(obj) + '\n')
+
+    # find next link
+
+    # if new day has started a new request has to be made
+    if timeNow.hour == 0:
+        # find day table
+        allDays = soup.find("table", "progDays").find_all("a")
+
+        # find current day of month
+        dayToFind = str(timeNow.day)
+
+        # look for current day
+        for day in allDays:
+            if dayToFind in day.text:
+                url = day['href']
+
+        # perform get request for new day
+        contents = session.get(url, headers=headers)
+
+        # read encoding of contents
+        encoding = contents.encoding if 'charset' in contents.headers.get(
+            'content-type', '').lower() else None
+
+        # cook up some new stew
+        soup = BeautifulSoup(
+            contents.content, from_encoding=encoding, features="html.parser")
+
+    # time period for next run
+    hourToFind = str(timeNow.hour) + ' - ' + \
+        str((timeNow + timedelta(hours=1)).hour)
+
+    # find all time periods
+    songTimes = soup.find("div", "progTime pulldown").find_all("a")
+
+    # look for correct time period
+    for songTime in songTimes:
+        if hourToFind in songTime.text:
+            # write time period to file
+            with open('nextHour.txt', 'w') as f:
+                print(songTime['href'], file=f)
