58ccca560f6a693d07b503ff8357305ca7365962
RAlbiez <r.albiez@web.de>
2020-05-25 20:13:44 +0200

    final working version, added start and endtime to settings

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # SWR1SongData
 Read play statistics from SWR1 song history.
 
+Requires a Settings file named Settings.txt in same folder as the script, see SampleSettings file.
+
 
--- run.py
+++ run.py
@@ -3,7 +3,6 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-import re
 import os
 import json
 from types import SimpleNamespace as Namespace
@@ -17,12 +16,12 @@ with open(os.path.join(os.getcwd(), "Settings.txt"), 'r') as file:
     sett = file.readlines()
 
 for setting in sett:
-    jsonContent = setting.split('=')
-    settings[jsonContent[0]] = jsonContent[1]
+    line = setting.split('=')
+    settings[line[0]] = line[1].replace('\n', '')
 
 # link for lastFM song API
 APILink = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&autocorrect=1&api_key=' + \
-    settings['API key'].replace('\n', '')
+    settings['API key']
 
 songMetadata = {}
 
@@ -74,7 +73,7 @@ class entry:
                     tags.append(tag.name)
 
         # build output
-        outputArgs = [self.date.strftime('%y.%m.%d_%H:%M'), self.songName, self.artist, albumName, length, "[" + ','.join(tags) + "]"]
+        outputArgs = [self.date.strftime('%y_%m_%d_%H:%M'), self.songName, self.artist, albumName, length, "[" + ','.join(tags) + "]"]
 
         # return result
         return ';'.join(outputArgs)
@@ -85,75 +84,80 @@ class entry:
     title = ''
     date = ''
 
-urlDate = datetime.strptime("01.01.2019",'%d.%m.%Y')
+urlDate = datetime.strptime(settings['StartDate'],'%d.%m.%Y:%H')
 
-while urlDate.year == 2019:
+while urlDate.strftime('%d.%m.%Y:%H') != settings['EndDate']:
     print(urlDate)
-    for hour in range(24):
 
-        urlDate = urlDate.replace(hour = hour)
+    url = 'https://www.swr.de/swr1/bw/playlist/index.html?time=' + urlDate.strftime('%H') + '%3A00&date=' + urlDate.strftime('%Y-%m-%d')
 
-        url = 'https://www.swr.de/swr1/bw/playlist/index.html?time=' + urlDate.strftime('%H') + '%3A00&date=' + urlDate.strftime('%Y-%m-%d')
+    # header for request
+    headerString = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
+    Chrome / 41.0 .2272 .101 Safari / 537.36 '
 
-        # header for request
-        headerString = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
-        Chrome / 41.0 .2272 .101 Safari / 537.36 '
+    # new session for request
+    session = requests.Session()
 
-        # new session for request
-        session = requests.Session()
+    headers = {'User-Agent': headerString}
 
-        headers = {'User-Agent': headerString}
+    # perform get request
+    contents = session.get(url, headers=headers)
 
-        # perform get request
-        contents = session.get(url, headers=headers)
+    # read encoding of contents
+    encoding = contents.encoding if 'charset' in contents.headers.get(
+        'content-type', '').lower() else None
 
-        # read encoding of contents
-        encoding = contents.encoding if 'charset' in contents.headers.get(
-            'content-type', '').lower() else None
+    # create BeatifulSoup Html reader instance with correct encoding
+    soup = BeautifulSoup(
+        contents.content, from_encoding=encoding, features="html.parser")
 
-        # create BeatifulSoup Html reader instance with correct encoding
-        soup = BeautifulSoup(
-            contents.content, from_encoding=encoding, features="html.parser")
+    # find all li tags from class "musicListItem"
+    items = soup.find_all("div", {"class": "list-playlist-item"}, "dd")
 
-        # find all li tags from class "musicListItem"
-        items = soup.find_all("div", {"class": "list-playlist-item"}, "dd")
+    # get the text without html tags contained in these items
+    results = [i.text for i in items]
 
-        # get the text without html tags contained in these items
-        results = [i.text for i in items]
+    # parse the result into  entry objects
+    songEntrys = []
+    for unchangedData in results:
+        # split line at newline char to find the relevant lines
+        dataLines = unchangedData.split('\n')
+        relevantLines = []
+        for line in dataLines:
+            # remove whitespace
+            lineStripped = line.strip()
+            # add line to result if there is still text in line
+            if lineStripped != '':
+                relevantLines.append(lineStripped)
+        
+        date = datetime.strptime(relevantLines[0],'%d.%m.%Y %H:%M')
 
-        # parse the result into  entry objects
-        songEntrys = []
-        for unchangedData in results:
-            # split line at newline char to find the relevant lines
-            dataLines = unchangedData.split('\n')
-            relevantLines = []
-            for line in dataLines:
-                # remove whitespace
-                lineStripped = line.strip()
-                # add line to result if there is still text in line
-                if lineStripped != '':
-                    relevantLines.append(lineStripped)
+        if date.hour == urlDate.hour:
+            # create entry object for line
+            if len(relevantLines) > 1:
+                songName = relevantLines[2]
+            else:
+                songName = 'Unknown Song'
             
-            date = datetime.strptime(relevantLines[0],'%d.%m.%Y %H:%M')
+            if len(relevantLines) > 4:
+                 songArtist = relevantLines[4]
+            else:
+                songArtist = 'Unknown Artist'
 
-            if date.hour == urlDate.hour:
-                # create entry object for line
-                songName = relevantLines[2] if len(relevantLines) > 1 and len(relevantLines[2]) > 0 else 'Unknown Song'
-                songArtist = relevantLines[4] if len(relevantLines) > 3 and len(relevantLines[4]) > 0 else 'Unknown Artist'
-                obj = entry(songName, songArtist, date)
-                songEntrys.append(obj)
+            obj = entry(songName, songArtist, date)
+            songEntrys.append(obj)
 
-        # make a new file every month
-        resultFileName = 'SWR1_History_' + \
-            str(urlDate.year) + '_' + str(urlDate.month) + '.txt'
+    # make a new file every month
+    resultFileName = 'SWR1_History_' + \
+        str(urlDate.year) + '_' + str(urlDate.month) + '.txt'
 
-        # join file path
-        resultFilePath = os.path.join(os.path.expanduser(
-            '~'), settings['path'], resultFileName)
+    # join file path
+    resultFilePath = os.path.join(os.path.expanduser(
+        '~'), settings['path'], resultFileName)
 
-        # print result to file
-        with open(resultFilePath.replace('\n', ''), 'a') as file:
-            for obj in songEntrys:
-                file.write(str(obj) + '\n')
-
-    urlDate = urlDate + timedelta(days = 1)
+    # print result to file
+    with open(resultFilePath.replace('\n', ''), 'a', encoding='utf-8') as file:
+        for obj in songEntrys[::-1]:
+            file.write(str(obj) + '\n')
+    
+    urlDate = urlDate + timedelta(hours=1)
